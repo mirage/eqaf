d14f59bec4f8b34ae7fb45941ceedbfd1444e15a
Calascibetta Romain <romain.calascibetta@gmail.com>
2018-08-30 16:45:46 +0200

    Fix a C bug ... (int differ from a intnat)

--- lib/safeq.c
+++ lib/safeq.c
@@ -5,7 +5,9 @@ eqaf(value s1, value s2)
 {
   mlsize_t ln = Wosize_val(s1); /* assert (Wosize_val(s1) == Wosize_val(s2)); */
   value * p1, * p2;
-  int rt = 0;
+  value rt = 0;
+
+  if (ln != Wosize_val(s2)) return Val_false;
 
   for (p1 = Op_val(s1), p2 = Op_val(s2); ln > 0; ln--, p1++, p2++)
     rt |= *p1 ^ *p2;
