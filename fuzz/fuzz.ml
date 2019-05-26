5c8c36da2024a2d669491c8164a4524f03e66982
Calascibetta Romain <romain.calascibetta@gmail.com>
2019-05-26 14:14:25 +0200

    Add fuzzer on compare_{be,le}

--- fuzz/fuzz.ml
+++ fuzz/fuzz.ml
@@ -5,3 +5,23 @@ let () =
   let expect = String.equal a b in
   let result = Eqaf.equal a b in
   check_eq ~pp:Format.pp_print_bool ~eq:(=) expect result
+
+let rev str =
+  let len = String.length str in
+  let res = Bytes.create len in
+  for i = 0 to len - 1 do Bytes.set res (len - 1 - i) str.[i] done ;
+  Bytes.unsafe_to_string res
+
+let () =
+  add_test ~name:"compare_le" [ bytes; bytes ] @@ fun a b ->
+  if String.length a <> String.length b then bad_test () ;
+  let expect = String.compare a b in
+  let result = Eqaf.compare_le a b in
+  check_eq ~pp:Format.pp_print_int ~eq:(=) expect result
+
+let () =
+  add_test ~name:"compare_be" [ bytes; bytes ] @@ fun a b ->
+  if String.length a <> String.length b then bad_test () ;
+  let expect = String.compare (rev a) (rev b) in
+  let result = Eqaf.compare_be a b in
+  check_eq ~pp:Format.pp_print_int ~eq:(=) expect result
