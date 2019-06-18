aa282f1cb1ec84b0b575ed23cbf56fcb232ba230
Calascibetta Romain <romain.calascibetta@gmail.com>
2019-06-18 11:10:36 +0200

    Optimization on compare_{be,le} functions (@cfcs)

--- fuzz/fuzz.ml
+++ fuzz/fuzz.ml
@@ -12,16 +12,23 @@ let rev str =
   for i = 0 to len - 1 do Bytes.set res (len - 1 - i) str.[i] done ;
   Bytes.unsafe_to_string res
 
+type order = Zero | Neg | Pos
+
+let of_int = function 0 -> Zero | n -> if n < 0 then Neg else Pos
+
+let pf = Format.fprintf
+let pp_order ppf = function Zero -> pf ppf "Zero" | Neg -> pf ppf "Neg" | Pos -> pf ppf "Pos"
+
 let () =
   add_test ~name:"compare_le" [ bytes; bytes ] @@ fun a b ->
   if String.length a <> String.length b then bad_test () ;
   let expect = String.compare a b in
   let result = Eqaf.compare_be a b in
-  check_eq ~pp:Format.pp_print_int ~eq:(=) expect result
+  check_eq ~pp:pp_order ~eq:(=) (of_int expect) (of_int result)
 
 let () =
   add_test ~name:"compare_be" [ bytes; bytes ] @@ fun a b ->
   if String.length a <> String.length b then bad_test () ;
   let expect = String.compare (rev a) (rev b) in
   let result = Eqaf.compare_le a b in
-  check_eq ~pp:Format.pp_print_int ~eq:(=) expect result
+  check_eq ~pp:pp_order ~eq:(=) (of_int expect) (of_int result)
