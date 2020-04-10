6afa45e7a1c60f54937d6f4e3be73657c7724dc1
Calascibetta Romain <romain.calascibetta@gmail.com>
2020-04-10 13:00:24 +0200

    Fix windows support of mtime

--- check/mtime/select/select.ml
+++ check/mtime/select/select.ml
@@ -1,7 +1,7 @@
 let invalid_arg fmt = Format.ksprintf (fun s -> invalid_arg s) fmt
 
 let load_file filename =
-  let ic = open_in filename in
+  let ic = open_in_bin filename in
   let ln = in_channel_length ic in
   let rs = Bytes.create ln in
   let () = really_input ic rs 0 ln in
