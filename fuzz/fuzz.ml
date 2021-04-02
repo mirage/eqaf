f71c80271f30628fb73e6df7234cd6106ed8a066
Your Name <you@example.com>
2021-04-02 13:09:58 +0200

    Add some utility functions
    This commit proposes a number of constant time utility functions:
    - `divmod`, unsigned 32bit division with small divisors
    - `ascii_of_int32`, conversion from `int32` to decimal `string` representation
    - `select_a_if_in_range`, like `select_int` but supporting positive ranges. In the future we can perhaps devise an algorithm to support arbitrary ranges.
    - `lowercase_ascii` and `uppercase_ascii`, CT implementations of `String.(lowercase_ascii,uppercase_ascii` using `select_a_if_in_range`
    - `hex_of_bytes` / `hex_of_string`, hex encoding
    - `bytes_of_hex` / `string_of_hex`, hex decoding
    - `bool_of_int` / `int_of_bool` (CT `Bool.to_int`) for constant time `bool`/`int` conversions
    
    The doc generation with `dune build @doc` is also fixed to not generate errors, and `eqaf.mli` (the docs) were reorganized into sections to be easier to read.
    
    The `README.md` was also slightly rewritten to document the existence of the other functions exposed by the library.
    
    The additions are integrated with the fuzzing infrastructure, and have been tested with 300 million executions FWIW.

--- fuzz/fuzz.ml
+++ fuzz/fuzz.ml
@@ -1,6 +1,67 @@
 open Crowbar
 
 let () =
+  add_test ~name:"select_a_if_in_range"
+    [ range max_int ; range max_int ; int; range 10000; range 10000 ]
+  @@ fun low high n a b ->
+  let low, high = min low high, max low high in
+  let choice = Eqaf.select_a_if_in_range ~low ~high ~n a b in
+  check_eq ~eq:(=) (if low <= n && n <= high then a else b) choice
+
+let () =
+  add_test ~name:"divmod" [ int32 ; int32 ]
+  @@ fun x m ->
+  try
+    let result = Eqaf.divmod ~x ~m in
+    let expect = Int32.unsigned_div x m,
+                 Int32.unsigned_rem x m in
+    check_eq ~eq:(=) expect result
+  with
+  | Invalid_argument desc ->
+    (* we expect this for negative m: *)
+    assert (desc = "m <= 0" || desc = "m >= 16348 not supported")
+
+let () =
+  add_test ~name:"hex_of_string |> string_of_hex" [ bytes ]
+  @@ fun raw ->
+  let enc = Eqaf.hex_of_string raw in
+  let dec = Eqaf.string_of_hex enc in
+  check_eq ~pp:(fun fmt s -> Format.pp_print_string fmt @@ String.escaped s)
+    ~eq:(=) raw dec ;
+  String.iter (function (* check for invalid encoding: *)
+      | '0'..'9'
+      | 'a'..'z'
+      | 'A'..'Z' -> ()
+      | _ -> assert false) enc
+
+let () =
+  add_test ~name:"string_of_hex |> hex_of_string" [ bytes ]
+  @@ fun hex ->
+  begin
+    try begin
+      let dec = Eqaf.string_of_hex hex in
+      let enc = Eqaf.hex_of_string dec in
+      check_eq ~pp:(fun fmt s -> Format.pp_print_string fmt @@ String.escaped s)
+        ~eq:(=) (String.lowercase_ascii hex) enc ;
+    end
+    with Invalid_argument x ->
+      let invalid = ref false in
+      begin
+        if (String.length hex mod 2 = 1)
+        && x = "hex length must be multiple of 2" then begin
+          invalid := true
+        end else begin if x = "decoding invalid hex" then
+            String.iter (function
+                | 'a'..'f'
+                | '0'..'9'
+                | 'A'..'F' -> ()
+                | _ -> invalid := true
+              ) hex ;
+        end ;
+      end ; assert !invalid (* we expect it to be invalid since it raised *)
+  end
+
+let () =
   add_test ~name:"equal" [ bytes; bytes; ] @@ fun a b ->
   let expect = String.equal a b in
   let result = Eqaf.equal a b in
